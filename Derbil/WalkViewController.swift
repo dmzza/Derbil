dfa7c58814146e43a27bc0189a88894a5d78541f
David Mazza <david@elevateapp.com>
2015-08-03 23:52:04 -0700

    Attempted a walking animation

--- Derbil/WalkViewController.swift
+++ Derbil/WalkViewController.swift
@@ -33,11 +33,41 @@ class WalkViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         self.timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("tick"), userInfo: nil, repeats: true)
+        let rightFoot: UIView = UIView(frame: CGRectMake(0, self.view.bounds.size.height - 20, 60, 10))
+        let leftFoot: UIView = UIView(frame: CGRectMake(0, self.view.bounds.size.height - 80, 60, 10))
+        
+        rightFoot.backgroundColor = UIColor.blackColor()
+        leftFoot.backgroundColor = UIColor.blackColor()
+        rightFoot.alpha = 0.0
+        leftFoot.alpha = 0.0
+        self.view.addSubview(rightFoot)
+        self.view.addSubview(leftFoot)
+        
+        dispatch_after(1, dispatch_get_main_queue()) { () -> Void in
+            self.loop(rightFoot)
+        }
+        dispatch_after(400000, dispatch_get_main_queue()) { () -> Void in
+            self.loop(leftFoot)
+        }
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loop(view: UIView) {
+        
+        UIView.animateWithDuration(1.9, animations: { () -> Void in
+            view.transform = CGAffineTransformMakeTranslation(self.view.bounds.width, 0)
+            view.alpha = 1.0
+            }) { (finished: Bool) -> Void in
+                view.transform = CGAffineTransformMakeTranslation(-1 * view.bounds.width, 0)
+                self.loop(view)
+        }
     }
     
     // MARK: Actions
