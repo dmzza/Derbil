0a0fe4aea94e4451f62fe63ff40a64c87444abed
David Mazza <davidedwardmazza@gmail.com>
2015-09-13 13:18:42 -0700

    Started on walking with the watch

--- Derbil WatchKit Extension/InterfaceController.swift
+++ Derbil WatchKit Extension/InterfaceController.swift
@@ -12,12 +12,31 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var actionButton: WKInterfaceButton!
+    @IBOutlet var pauseGroup: WKInterfaceGroup!
+    
+    @IBOutlet var actionNameLabel: WKInterfaceLabel!
+    @IBOutlet var walkTimer: WKInterfaceTimer!
+    var walking: Bool = false {
+        didSet {
+            if walking {
+                actionButton.setHidden(false)
+                pauseGroup.setHidden(true)
+                actionNameLabel.setHidden(true)
+                walkTimer.setHidden(false)
+                walkTimer.start()
+            } else {
+                walkTimer.stop()
+            }
+        }
+    }
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
         // Configure interface objects here.
     }
-
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
@@ -27,5 +46,31 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    @IBAction func didAct() {
+        if walking {
+            // Pause
+            actionButton.setHidden(true)
+            pauseGroup.setHidden(false)
+            walking = false
+        } else {
+            // Start
+            walkTimer.setDate(NSDate())
+            walking = true
+        }
+    }
+
+    @IBAction func resume() {
+        walking = true
+    }
+    
+    @IBAction func end() {
+        actionButton.setHidden(false)
+        pauseGroup.setHidden(true)
+        actionNameLabel.setHidden(false)
+        walkTimer.setHidden(true)
+        walking = false
+    }
+    
 
 }
