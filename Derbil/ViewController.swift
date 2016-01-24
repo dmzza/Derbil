aeea47f35b63736fd8b4a569a20dfb2e1a8febfb
David Mazza <github@dmzza.com>
2016-01-24 14:52:19 -0800

    Close EatVC after picking a meal from the new button

--- Derbil/ViewController.swift
+++ Derbil/ViewController.swift
@@ -14,7 +14,7 @@ let kHappyWalkThreshold: NSTimeInterval = 120
 let kPressHeadDuration: NSTimeInterval = 0.08
 
 class ViewController: UIViewController, WalkViewControllerDelegate, EatViewControllerDelegate, UIGestureRecognizerDelegate, UIDynamicAnimatorDelegate, DialogManagerDelegate {
-  
+
   @IBOutlet weak var faceContainer: UIView!
   var faceView: FaceView?
   @IBOutlet var speechBubble: UIView!
@@ -23,7 +23,7 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
   @IBOutlet var responseBubbleLabel: UILabel!
   @IBOutlet var responseBubbleImage: UIImageView!
   @IBOutlet var eatButton: UIButton!
-  
+
   var animator: UIDynamicAnimator?
   var pushHeadBehavior: UIPushBehavior?
   var headSnapBehavior: UISnapBehavior?
@@ -53,23 +53,23 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
   var hueStartingPoint:CGFloat = 0.0
   let bubbleFadeInDuration = 1.0
   let userDefaults = NSUserDefaults.standardUserDefaults()
-  
+
   var dialogManager: DialogManager?
   var responseCompletionBlock: ((didRespond: Bool, response: Any?) -> ())?
   var responseSentence: Sentence?
   var speechCompletionBlock: ((didSpeak: Bool) -> ())?
   var speechSentence: Sentence?
-  
+
   @IBOutlet var headView: UIImageView!
   @IBOutlet var firstHeart: UIImageView!
   @IBOutlet var secondHeart: UIImageView!
   @IBOutlet var thirdHeart: UIImageView!
-  
+
   @IBOutlet var loveButton: UIButton!
   @IBOutlet var headPressRecognizer: UILongPressGestureRecognizer!
   @IBOutlet var colorPanRecognizer: UIPanGestureRecognizer!
-  
-  
+
+
   let walksPerDay = 4
   var walks: Int = 0 {
     didSet {
@@ -78,7 +78,7 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
       self.thirdHeart.hidden = walks < 3
     }
   }
-  
+
   let notificationTitle = "Chubbyy needs love"
   let morningNotificationBody = "🌞"
   let morningNotificationThought = "Wake up! I need to pee, take me outside."
@@ -89,31 +89,31 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
   let morningNotificationTime = NSDate().change(hour: 9, minute: 0)
   let afternoonNotificationTime = NSDate().change(hour: 15, minute: 0)
   let eveningNotificationTime = NSDate().change(hour: 21, minute: 0)
-  
+
   override func awakeFromNib() {
     super.awakeFromNib()
-    
+
     let face: Face = Face(
       mouth: Face.Name.MouthName(Face.Mouth.Puppy, Face.Part.Mouth),
       leftEye: Face.Name.EyeName(Face.Eye.Normal, Face.Part.Left),
       rightEye: Face.Name.EyeName(Face.Eye.Normal, Face.Part.Right))
     let center = CGPointMake(self.view.frame.midX, self.view.frame.midY)
-    
+
     self.faceView = FaceView(face: face, frame: self.faceContainer.bounds)
     self.faceContainer.addSubview(self.faceView!)
-    
+
     let faceSnapBehavior = UISnapBehavior(item: self.faceView!, snapToPoint: center)
-    
+
     self.pushHeadBehavior = UIPushBehavior(items: [self.faceView!, self.headView], mode: .Continuous)
     self.pushHeadBehavior!.pushDirection = CGVectorMake(0, 0)
     self.pushHeadBehavior!.magnitude = 0
     self.animator!.addBehavior(faceSnapBehavior)
     self.animator!.addBehavior(self.pushHeadBehavior!)
   }
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+
     self.scheduleNotifications()
     NSNotificationCenter.defaultCenter().addObserverForName(kInAppNotificationReceived,
       object: nil,
@@ -125,20 +125,20 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
           }
         }
     })
-    
+
     NSNotificationCenter.defaultCenter().addObserverForName(kNotificationNameNewDayBegan,
       object: nil,
       queue: NSOperationQueue.mainQueue()) { (note) -> Void in
         self.userDefaults.setInteger(0, forKey: kUserDefaultsTodaysWalkCount)
     }
-    
+
     NSNotificationCenter.defaultCenter().addObserverForName(kNotificationNameLuckyNumberReceived,
         object: nil,
         queue: NSOperationQueue.mainQueue()) { (note) -> Void in
             let luckyNumber = ((note.userInfo! as Dictionary)[kNotificationUserInfoKeyLuckyNumber])! as! UInt
             // do nothing yet
     }
-    
+
     NSNotificationCenter.defaultCenter().addObserverForName(kNotificationNameDeviceShaken,
         object: nil,
         queue: NSOperationQueue.mainQueue()) { (note) -> Void in
@@ -147,47 +147,47 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
             vc.delegate = self
             self.presentViewController(vc, animated: true, completion: nil)
     }
-    
+
     self.walks = userDefaults.integerForKey(kUserDefaultsTodaysWalkCount)
     self.animator = UIDynamicAnimator(referenceView: self.view)
     self.animator!.delegate = self
     self.revertToSavedHeadColor()
     self.headPressRecognizer.delegate = self
-    
+
     self.speechBubble.alpha = 0.0
     self.responseBubble.alpha = 0.0
-    
+
     self.dialogManager = DialogManager(delegate: self)
-    
+
     self.eatButton.layer.cornerRadius = 23.0
   }
-  
+
   override func viewDidAppear(animated: Bool) {
     super.viewDidAppear(animated)
     if (self.headSnapBehavior == nil) {
       self.headSnapBehavior = UISnapBehavior(item: self.headView, snapToPoint: CGPointMake(self.headView.frame.midX, self.headView.frame.midY))
-      
+
       self.animator!.addBehavior(self.headSnapBehavior!)
       self.responseBubbleImage.image = flippedBubbleImage()
     }
   }
-  
+
   func scheduleNotifications() {
     let morningNotification = self.notification(morningNotificationTime, body: morningNotificationBody, title: notificationTitle, thought:morningNotificationThought)
     let afternoonNotification = self.notification(afternoonNotificationTime, body: afternoonNotificationBody, title: notificationTitle, thought:morningNotificationThought)
     let eveningNotification = self.notification(eveningNotificationTime, body: eveningNotificationBody, title: notificationTitle, thought:morningNotificationThought)
-    
+
     UIApplication.sharedApplication().registerUserNotificationSettings(UIUserNotificationSettings(forTypes: UIUserNotificationType.Alert, categories: nil))
     UIApplication.sharedApplication().cancelAllLocalNotifications()
     UIApplication.sharedApplication().scheduleLocalNotification(morningNotification)
     UIApplication.sharedApplication().scheduleLocalNotification(afternoonNotification)
     UIApplication.sharedApplication().scheduleLocalNotification(eveningNotification)
   }
-  
+
   func notification(timeOfDay: NSDate, body: String, title: String, thought: String) -> UILocalNotification {
     let note = UILocalNotification()
     let now = NSDate()
-    
+
     note.alertBody = body
     note.alertTitle = title
     note.userInfo = ["thought": thought]
@@ -200,7 +200,7 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
     print("\(note.fireDate?.description)")
     return note
   }
-  
+
   func smile(duration: NSTimeInterval) {
     if let view: FaceView = self.faceView {
       let face: Face = self.faceView!.face
@@ -213,12 +213,12 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
       }
     }
   }
-  
+
   func speak(thoughts: String) {
     self.speechBubbleLabel.text = thoughts
   }
-  
-  
+
+
   @IBAction func sendResponse(sender: AnyObject) {
     if let completion = self.responseCompletionBlock {
       let slideUp = CGAffineTransformMakeTranslation(0, -100)
@@ -239,10 +239,10 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
           self.responseCompletionBlock = nil
           self.responseSentence = nil
       })
-      
+
     }
   }
-  
+
   @IBAction func changeColor(sender: UIPanGestureRecognizer) {
     let deltaY = sender.translationInView(self.view).y / self.view.bounds.size.height
     var sat: CGFloat = 0.0
@@ -277,7 +277,7 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
       break
     }
   }
-  
+
   @IBAction func eatMeal(sender: AnyObject) {
     if self.isSleeping {
       self.pressHead()
@@ -288,12 +288,12 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
       mouth: Face.Name.MouthName(Face.Mouth.Laughing, Face.Part.Mouth),
       leftEye: Face.Name.EyeName(Face.Eye.Normal, Face.Part.Left),
       rightEye: Face.Name.EyeName(Face.Eye.Normal, Face.Part.Right))
-    
+
     for i in 1...10 {
       NSTimer.scheduledTimerWithTimeInterval(0.05 * Double(i), target: self, selector: "giveHeart", userInfo: nil, repeats: false)
     }
   }
-  
+
   @IBAction func tapHead(sender: UILongPressGestureRecognizer) {
     switch sender.state {
     case .Began:
@@ -309,7 +309,7 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
       break
     }
   }
-  
+
   @IBAction func tapBackground(sender: UITapGestureRecognizer) {
     switch sender.state {
     case .Ended:
@@ -319,7 +319,7 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
       break
     }
   }
-  
+
   func pressHead() {
     if let completion = self.speechCompletionBlock {
       completion(didSpeak: true)
@@ -332,25 +332,25 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
       })
     }
   }
-  
+
   func moveHead(x: CGFloat, y: CGFloat) {
     self.pushHeadBehavior!.pushDirection = CGVector(dx: x, dy: y)
     self.pushHeadBehavior!.magnitude = abs(x + y)
   }
-  
+
   func releaseHead() {
     self.pushHeadBehavior!.pushDirection = CGVectorMake(0, 0)
     self.pushHeadBehavior!.magnitude = 0
   }
-  
+
   func giveHeart() {
     let randomStartingPoint = CGPointMake(CGFloat(arc4random()) % 6000.0 - 3000.0, CGFloat(arc4random()) % 10000.0 - 5000.0)
     self.giveHeartFromOrigin(randomStartingPoint)
   }
-  
+
   func giveHeartFromOrigin(origin: CGPoint) {
     let heart: UIImageView = UIImageView(image: UIImage(named: "tiny-heart"))
-    
+
     heart.frame = CGRect(origin: origin, size: self.loveButton.bounds.size)
     heart.tintColor = UIColor.redColor()
     self.view.addSubview(heart)
@@ -360,70 +360,76 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
       }) { (finished) -> Void in
         self.animator!.removeBehavior(behavior)
     }
-    
+
     let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(2 * Double(NSEC_PER_SEC)))
     dispatch_after(delayTime, dispatch_get_main_queue()) {
       heart.removeFromSuperview()
     }
     self.animator!.addBehavior(behavior)
-    
+
     if let completion = self.speechCompletionBlock {
       completion(didSpeak: true)
       self.speechCompletionBlock = nil
       self.speechSentence = nil
     }
   }
-  
+
   func revertToSavedHeadColor() {
     if let savedHeadColor: NSData = userDefaults.objectForKey(kUserDefaultsHeadColor) as? NSData {
       self.headColor = NSKeyedUnarchiver.unarchiveObjectWithData(savedHeadColor) as! UIColor
     }
   }
-  
+
   @IBAction func didTripleTap(sender: UITapGestureRecognizer) {
     let instaNote: UILocalNotification = self.notification(NSDate() + 5.seconds, body: "😖", title: notificationTitle, thought: "Hello")
     UIApplication.sharedApplication().scheduleLocalNotification(instaNote)
   }
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    if segue.identifier == "WalkViewControllerSegue" {
-      let vc: WalkViewController = (segue.destinationViewController as! UINavigationController).topViewController as! WalkViewController
-      vc.delegate = self
-    }
-  }
-  
+
   func didComplete(controller: WalkViewController, elapsedTime: NSTimeInterval) {
     if elapsedTime >= kHappyWalkThreshold {
       self.smile(elapsedTime / 10)
       let userDefaults = NSUserDefaults.standardUserDefaults()
       let currentWalks: Int = userDefaults.integerForKey(kUserDefaultsWalkCount)
       let todaysWalks: Int = userDefaults.integerForKey(kUserDefaultsTodaysWalkCount)
-      
+
       self.walks = todaysWalks + 1
       userDefaults.setInteger(currentWalks + 1, forKey: kUserDefaultsWalkCount)
       userDefaults.setInteger(self.walks, forKey: kUserDefaultsTodaysWalkCount)
-      
+
     }
   }
-    
-  // mark - EatViewControllerDelegate
-  
+
+  // MARK: Navigation
+
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if let modalVC = segue.destinationViewController as? EatViewController {
+      modalVC.delegate = self
+    }
+    if segue.identifier == "WalkViewControllerSegue" {
+      let vc: WalkViewController = (segue.destinationViewController as! UINavigationController).topViewController as! WalkViewController
+      vc.delegate = self
+    }
+  }
+
+  // MARK: EatViewControllerDelegate
+
   func eatViewControllerDidDismiss(vc: EatViewController) {
     self.dismissViewControllerAnimated(true) { () -> Void in
       self.giveHeart()
     }
   }
-  
-  // mark - UIGestureRecognizerDelegate
-  
+
+  // MARK: UIGestureRecognizerDelegate
+
   func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
     if otherGestureRecognizer is UIPanGestureRecognizer {
       return true
     }
     return false
   }
-  
-  // mark - UIDynamicAnimatorDelegate
-  
+
+  // MARK: UIDynamicAnimatorDelegate
+
   func dynamicAnimatorDidPause(animator: UIDynamicAnimator) {
     if !self.isSleeping {
       self.faceView!.face = Face(
@@ -432,9 +438,9 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
         rightEye: Face.Name.EyeName(Face.Eye.Normal, Face.Part.Right))
     }
   }
-  
-  // mark - DialogManagerDelegate 
-  
+
+  // MARK: DialogManagerDelegate
+
   func dialogManager(manager: DialogManager, wantsChubbyyToSpeak sentence: Sentence, completion: (didSpeak: Bool) -> ()) {
     self.speechBubbleLabel.text = sentence.text
     self.speechBubble.transform = CGAffineTransformMakeTranslation(0.0, 100.0)
@@ -448,7 +454,7 @@ class ViewController: UIViewController, WalkViewControllerDelegate, EatViewContr
         self.speechSentence = sentence
     }
   }
-  
+
   func dialogManager(manager: DialogManager, wantsUserToRespond sentence: Sentence, completion: (didRespond: Bool, response: Any?) -> ()) {
     var sentenceText = sentence.text!
     if sentence.responseType == ResponseType.Number {
